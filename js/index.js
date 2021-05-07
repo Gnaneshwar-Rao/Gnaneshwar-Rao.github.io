ade351bc26fd3abd0a06e7e83a52dce381bfd27a
Gnaneshwar-Rao <69629661+Gnaneshwar-Rao@users.noreply.github.com>
2021-05-07 16:37:58 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD6dPtdnSg2Im_5CyAFuUKB-rR9tF8zfPw",
+  authDomain: "rc4-ascension-2021.firebaseapp.com",
+  projectId: "rc4-ascension-2021",
+  storageBucket: "rc4-ascension-2021.appspot.com",
+  messagingSenderId: "463448717557",
+  appId: "1:463448717557:web:5ca0eaf3ad5bc62a232dbc",
+  measurementId: "G-YMNFSD28V4"
+};
+
+//Initialize firebase
+firebase.initializeApp(firebaseConfig);
